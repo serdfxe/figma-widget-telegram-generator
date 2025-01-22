4a049bf7218999f046124d10446f7902cc2823e3
JimGitFE <rymsjfjrys@gmail.com>
2025-01-22 19:45:10 -0300

    doc: improved dynamic state hook tsdoc vocabulary

--- widget-src/hooks/useDynamicState.ts
+++ widget-src/hooks/useDynamicState.ts
@@ -1,14 +1,18 @@
 const { useSyncedState } = figma.widget
 
-/** Dynamic Object of unknown values */
+/** Initial Default State (Dynamic object of unknown values) */
 export type DynamicState = { [key: string]: unknown }
-/** setState Callback (Only valid keys with matching values for initialState)
+
+/** setState Callback (Dynamic State & type safe)
+ * Only valid keys with matching values for initialState
  * @param propKey - Accepts keys from `initialState`
  * @param propValue - Values of matching type for `initialState[propKey]`
  */
-export type DynamicSetSTate<S> = <K extends keyof S>(propKey: K, propValue: S[K]) => void
+export type SetCallback<S> = <K extends keyof S>(propKey: K, propValue: S[K]) => void
 
-/** Centralized safely typed synced state object manager (example: hold multiple input values)
+/**
+ * Centralized safely typed synced state object manager (example: hold multiple input values)
+ *
  * @param initialState - `useSyncedState` default Value
  * @returns [state, setSafeState]
  *
@@ -25,11 +29,11 @@ export type DynamicSetSTate<S> = <K extends keyof S>(propKey: K, propValue: S[K]
  * setInputs("buttons", [{ id: 1, text: "Updated Button 1" }, { id: 2, text: "Updated Button 2" }])
  * console.log(inputs.buttons[0].text) // Output: "Updated Button 1"
  */
-export default function useDynamicState<S extends DynamicState>({ ...initialState }: S): [S, DynamicSetSTate<S>] {
+export default function useDynamicState<S extends DynamicState>({ ...initialState }: S): [S, SetCallback<S>] {
    const [state, setState] = useSyncedState<S>("state", initialState)
 
    /** Update state for specific state property */
-   const setSafeState: DynamicSetSTate<S> = (propKey, propValue) => {
+   const setSafeState: SetCallback<S> = (propKey, propValue) => {
       setState((prevState) => ({
          ...prevState,
          [propKey]: propValue,
